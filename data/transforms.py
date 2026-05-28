71a5f2333442752161055e8ffa4e7cc9aa376081
Hania <haniazipser2004@gmail.com>
2026-05-28 16:23:58 +0200

    fix: change transformations for metrics learning

--- data/transforms.py
+++ data/transforms.py
@@ -1,8 +1,11 @@
 from torchvision import transforms
 
 def get_transforms(mode: str, image_size=224):
-    mean = [0.485, 0.456, 0.406] #CHECK MEGADESCIPTORS DATA!!
-    std  = [0.229, 0.224, 0.225]
+    mean_efficient_net = [0.485, 0.456, 0.406]
+    std_efficient_net  = [0.229, 0.224, 0.225]
+
+    mean_mega_descriptor = [0.5, 0.5, 0.5] #to be confirmed
+    std_mega_descriptor = [0.5, 0.5, 0.5]
 
     if mode == "baseline":
         train = transforms.Compose([
@@ -13,7 +16,13 @@ def get_transforms(mode: str, image_size=224):
                 transforms.ColorJitter(0.3, 0.3, 0.2, 0.1)
             ], p=0.8),
             transforms.ToTensor(),
-            transforms.Normalize(mean, std),
+            transforms.Normalize(mean_efficient_net, std_efficient_net),
+
+        ])
+        val = transforms.Compose([
+            transforms.Resize((image_size, image_size)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean_efficient_net, std_efficient_net),
         ])
 
     elif mode == "metric_learning":
@@ -25,16 +34,16 @@ def get_transforms(mode: str, image_size=224):
                 transforms.ColorJitter(0.3, 0.3, 0.2, 0.1)
             ], p=0.8),
             transforms.ToTensor(),
-            transforms.Normalize(mean, std), #maybe skip?
+            transforms.Normalize(mean_mega_descriptor, std_mega_descriptor)
+        ])
+
+        val = transforms.Compose([
+            transforms.Resize((image_size, image_size)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean_mega_descriptor, std_mega_descriptor),
         ])
 
     else:
         raise ValueError(f"Unknown mode: {mode}")
-
-    val = transforms.Compose([
-        transforms.Resize((image_size, image_size)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean, std),
-    ])
 
     return train, val
