ba8ccb4889944820c9baa6a1ccf4a895d04b80d1
Cooke <stanislawkucharski205@gmail.com>
2026-06-05 09:20:19 +0200

    rerun baseline geo_split

--- config/presets/baseline.py
+++ config/presets/baseline.py
@@ -3,5 +3,5 @@ from config.config import DataConfig
 def get_config() -> DataConfig:
     return DataConfig(
         experiment_type="baseline",
-        split_type="time_open"
+        split_type="geo_aware"
     )
