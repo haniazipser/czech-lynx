df396a58c9aa1997b228b6d3bae035f6db71aed8
Cooke <stanislawkucharski205@gmail.com>
2026-06-05 09:21:12 +0200

    switched the config back to time split for future tuns

--- config/presets/baseline.py
+++ config/presets/baseline.py
@@ -3,5 +3,5 @@ from config.config import DataConfig
 def get_config() -> DataConfig:
     return DataConfig(
         experiment_type="baseline",
-        split_type="geo_aware"
+        split_type="time_open"
     )
