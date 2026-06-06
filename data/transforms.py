1550db88b1c819c1d314e8e0af28dd6b8f66f014
Cooke <stanislawkucharski205@gmail.com>
2026-06-06 15:00:21 +0200

    no flips run

--- data/transforms.py
+++ data/transforms.py
@@ -4,14 +4,14 @@ def get_transforms(mode: str, image_size=224):
     mean_efficient_net = [0.485, 0.456, 0.406]
     std_efficient_net  = [0.229, 0.224, 0.225]
 
-    mean_mega_descriptor = [0.5, 0.5, 0.5] #to be confirmed
+    mean_mega_descriptor = [0.5, 0.5, 0.5]
     std_mega_descriptor = [0.5, 0.5, 0.5]
 
     if mode == "baseline":
         train = transforms.Compose([
             transforms.Resize((image_size + 32, image_size + 32)),
             transforms.RandomCrop(image_size),
-            transforms.RandomHorizontalFlip(),
+            #transforms.RandomHorizontalFlip(),
             transforms.RandomApply([
                 transforms.ColorJitter(0.3, 0.3, 0.2, 0.1)
             ], p=0.8),
@@ -29,7 +29,7 @@ def get_transforms(mode: str, image_size=224):
         train = transforms.Compose([
             transforms.Resize((image_size + 32, image_size + 32)),
             transforms.RandomResizedCrop(image_size),
-            transforms.RandomHorizontalFlip(),
+            #transforms.RandomHorizontalFlip(),
             transforms.RandomApply([
                 transforms.ColorJitter(0.3, 0.3, 0.2, 0.1)
             ], p=0.8),
